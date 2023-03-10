e4ed9a5725a867cc3b5d6328cce4d122f3a540e8
HW4 <aliamachka@hotmail.com>
2023-03-10 10:25:10 -0800

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hogwarts harryPotter = new Hogwarts("Harry Potter", 11);
+        harryPotter.study();
+        harryPotter.playQuidditch();
     }
 }
